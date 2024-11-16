50eb9950ec38bbf8f880f366a767a193cafd7639
MissAV <sofia.sry7@gmail.com>
2024-11-17 04:54:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fungsi untuk memulai mining di setiap profil
+function startMining(profileId) {
+    // Misalkan kita buka tab baru untuk setiap profil (sebagai contoh)
+    const profileWindow = window.open('https://cryptotab.net', '_blank');
+    
+    // Dalam aplikasi nyata, Anda mungkin ingin menambahkan lebih banyak logika untuk menjalankan CryptoTab di setiap profil
+    console.log('Memulai mining di Profil ' + profileId);
+    
+    // Di sini Anda bisa memulai proses lain seperti VPN atau otentikasi CryptoTab
+}
